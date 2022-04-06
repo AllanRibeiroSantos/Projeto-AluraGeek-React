ece383073b83ae7a10fe9032de19461536f150c9
allanribeirosantos <allan_kaduh@hotmail.com>
2022-04-06 15:18:51 -0300

    updating

--- src/components/ListaProdutos/Cardimage.js
+++ src/components/ListaProdutos/Cardimage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './Cardimage.module.css'
 
-export default function Cardimage() {
+export default function Cardimage({ cardsrc }) {
   return (
       <div className={styles.card_container}>
-        <img className={styles.listaProdutos_cardimage} />
+        <img className={styles.listaProdutos_cardimage}
+        src={cardsrc} />
         <p className={styles.card_nome_produto}>Produto XYZ</p>
         <p className={styles.card_preco_produto}>R$ 60,00</p>
         <p className={styles.card_ver_produto}>Ver Produto</p>
--- src/components/ListaProdutos/ListaProdutosPaginaInicial.js
+++ src/components/ListaProdutos/ListaProdutosPaginaInicial.js
@@ -2,6 +2,14 @@ import React from 'react';
 import Cardimage from './Cardimage';
 import styles from './ListaProdutosPaginaInicial.module.css';
 import { AiOutlineArrowRight } from 'react-icons/ai';
+import list1src1 from '../../img/listadeprodutos/produto1.png'
+import list1src2 from '../../img/listadeprodutos/produto2.png'
+import list1src3 from '../../img/listadeprodutos/produto3.png'
+import list1src4 from '../../img/listadeprodutos/produto4.png'
+import list1src5 from '../../img/listadeprodutos/produto5.png'
+import list1src6 from '../../img/listadeprodutos/produto6.png'
+import list2src1 from '../../img/listadeprodutos/lista2produto1.png'
+import list2src2 from '../../img/listadeprodutos/lista2produto2.png'
 
 export default function ListaProdutosPaginaInicial({ categoriaProduto }) {
   return (
@@ -11,18 +19,18 @@ export default function ListaProdutosPaginaInicial({ categoriaProduto }) {
         <p>Ver tudo <AiOutlineArrowRight /></p>
       </div>
       <div className={styles.section_card_container}>
-        <Cardimage />
-        <Cardimage />
-        <Cardimage />
-        <Cardimage />
-        <Cardimage />
-        <Cardimage />
-        <Cardimage />
-        <Cardimage />
-        <Cardimage />
-        <Cardimage />
-        <Cardimage />
+        <Cardimage cardsrc={list1src1} />
+        <Cardimage cardsrc={list1src2} />
+        <Cardimage cardsrc={list1src3} />
+        <Cardimage cardsrc={list1src4} />
+        <Cardimage cardsrc={list1src5} />
+        <Cardimage cardsrc={list1src6} />
+        <Cardimage cardsrc={list2src1} />
+        <Cardimage cardsrc={list2src2} />
       </div>
+
+      {/* Devo fazer um loop para cada categoria (.section_container) devidamente com as suas informações colocadas em um json. Fazer também um loop para cada card. O json terá as informações de categoria e cardsrc. */}
+
     </section>
 
   )
